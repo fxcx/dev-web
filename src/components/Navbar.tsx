84a3eb8072a0d80f38f4a7c1c60ba14f9d373076
fxcx <facundo19burgos@gmail.com>
2024-04-24 19:08:28 -0300

    add handleNavigationChange function

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,6 @@ const Navbar = () => {
   return (
     <div className="absolute top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between flex-block px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
-        {/* <div className="relative">
-          <Image
-            src="/horseLogo.jpg"
-            alt="logo"
-            width={40}
-            height={40}
-            className="w-full h-full object-contain rounded-full"
-          />
-        </div> */}
         <h1 className="text-white text-[25px] font-semibold">
           facundo -{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
