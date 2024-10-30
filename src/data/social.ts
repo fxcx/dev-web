c0fc6796afb661312b027c5fb6f1199ca3633181
fxcx <facundo19burgos@gmail.com>
2024-10-30 16:41:49 -0300

    data the peoyectos,skills,social

---
+++ src/data/social.ts
@@ -0,0 +1,7 @@
+// data/socials.ts
+import { SocialLink } from '@/types/index';
+
+export const socials: SocialLink[] = [
+    { name: "github", src: "/github-color-svgrepo-com.svg", link: "https://github.com/fxcx" },
+    { name: "Linkedin", src: "/linkedin-svgrepo-com.svg", link: "https://www.linkedin.com/in/facundoburgos96/" },
+];
