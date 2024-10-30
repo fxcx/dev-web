8e0075a3ba1b3a3a5c41d30ca18cf1ff63c22893
fxcx <facundo19burgos@gmail.com>
2024-10-30 16:42:19 -0300

    type skill proyects,socials

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,124 +1,33 @@
-export const SkillData = [
-  {
-    name: "Html 5",
-    Image: "/html.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Css",
-    Image: "/css.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "JavaScript",
-    Image: "/js.png",
-    width: 65,
-    height: 65,
-  },
-  {
-    name: "Tailwind Css",
-    Image: "/tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "React",
-    Image: "/react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Postgresql",
-    Image: "/postgresql-svgrepo-com.svg",
-    width: 80,
-    height: 80,
-  },
+// types.ts
+import { IconType } from 'react-icons';
 
-  {
-    name: "TypeScript",
-    Image: "/ts.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Next js 13",
-    Image: "/next.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Python",
-    Image: "/python-svgrepo-com.svg",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Node js",
-    Image: "/node-js.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Mongo db",
-    Image: "/mongodb-svgrepo-com.svg",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Docker",
-    Image: "/docker.webp",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Docker",
-    Image: "/light-prisma-svgrepo-com.svg",
-    width: 80,
-    height: 80,
-  },
-  {
-    name: "Redis",
-    Image: "/redis-svgrepo-com.svg",
-    width: 80,
-    height: 80,
-  },
-  
-];
+// Tipo para las habilidades (skills)
+export interface Skill {
+  name: string;
+  image: string;
+  width: number;
+  height: number;
+}
 
-export const Socials = [
-  {
-    name: "github",
-    src: "/github-color-svgrepo-com.svg",
-    link:"https://github.com/fxcx",  
-  },
-  {
-    name: "Linkedin",
-    src: "/linkedin-svgrepo-com.svg",
-    link:"https://www.linkedin.com/in/facundoburgos96/",
-  },
-];
-export const Projects = [
-  {
-    title: "Modern Nextjs Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "",
-  },
-  {
-    title: "Space Themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "",
-  },
-  {
-    title: "Modern Nextjs Portfolio",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "/WebPortfolio.png",
-  },
-  {
-    title: "Matrix themed Website",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    src: "",
-  },
-];
+// Tipo para los enlaces de redes sociales
+export interface SocialLink {
+  name: string;
+  src: string;
+  link: string;
+}
 
+// Tipo para las etiquetas de los proyectos
+export interface Tag {
+  name: string;
+  icon: IconType;
+}
+
+// Tipo para los proyectos
+export interface Project {
+  id: number;
+  name: string;
+  src: string;
+  description: string;
+  tags: Tag[];
+  link: string;
+}
