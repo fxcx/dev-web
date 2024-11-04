362da4238ffb7479a1e872f611f88ad123ee7367
fxcx <facundo19burgos@gmail.com>
2024-11-04 16:32:07 -0300

    add icon gmail

--- src/data/social.ts
+++ src/data/social.ts
@@ -3,5 +3,6 @@ import { SocialLink } from '@/types/index'
 export const socials: SocialLink[] = [
   { name: 'github', src: '/social/github-color-svgrepo-com.svg', link: 'https://github.com/fxcx' },
   { name: 'Linkedin', src: '/social/linkedin-svgrepo-com.svg', link: 'https://www.linkedin.com/in/facundoburgos96/' },
+  { name: 'Email', src: '/social/gmail-svgrepo-com.svg', link: 'mailto:facundo19burgos@gmail.com' },
   { name: 'Gitlab', src: '/social/gitlab-logo-500.svg', link: 'https://gitlab.com/fxcx' }
 ]
