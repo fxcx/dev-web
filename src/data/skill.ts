cb2f5ca36c9edd458038a586ed1a643dcccc3de3
fxcx <facundo19burgos@gmail.com>
2024-11-04 15:44:40 -0300

    formated

--- src/data/skill.ts
+++ src/data/skill.ts
@@ -1,18 +1,18 @@
 import { Skill } from '@/types/index'
 
 export const skills: Skill[] = [
-    { name: 'Html 5', image: '/html.png', width: 80, height: 80 },
-    { name: 'Css', image: '/css.png', width: 80, height: 80 },
-    { name: 'JavaScript', image: '/js.png', width: 65, height: 65 },
-    { name: 'Tailwind Css', image: '/tailwind.png', width: 80, height: 80 },
-    { name: 'React', image: '/react.png', width: 80, height: 80 },
-    { name: 'Postgresql', image: '/postgresql-svgrepo-com.svg', width: 80, height: 80 },
-    { name: 'TypeScript', image: '/ts.png', width: 80, height: 80 },
-    { name: 'Next js 13', image: '/next.png', width: 80, height: 80 },
-    { name: 'Python', image: '/python-svgrepo-com.svg', width: 80, height: 80 },
-    { name: 'Node js', image: '/node-js.png', width: 80, height: 80 },
-    { name: 'Mongo db', image: '/mongodb-svgrepo-com.svg', width: 80, height: 80 },
-    { name: 'Docker', image: '/docker.webp', width: 80, height: 80 },
-    { name: 'Prisma', image: '/light-prisma-svgrepo-com.svg', width: 80, height: 80 },
-    { name: 'Redis', image: '/redis-svgrepo-com.svg', width: 80, height: 80 }
+  { name: 'Html 5', image: '/html.png', width: 80, height: 80 },
+  { name: 'Css', image: '/css.png', width: 80, height: 80 },
+  { name: 'JavaScript', image: '/js.png', width: 65, height: 65 },
+  { name: 'Tailwind Css', image: '/tailwind.png', width: 80, height: 80 },
+  { name: 'React', image: '/react.png', width: 80, height: 80 },
+  { name: 'Postgresql', image: '/postgresql-svgrepo-com.svg', width: 80, height: 80 },
+  { name: 'TypeScript', image: '/ts.png', width: 80, height: 80 },
+  { name: 'Next js 13', image: '/next.png', width: 80, height: 80 },
+  { name: 'Python', image: '/python-svgrepo-com.svg', width: 80, height: 80 },
+  { name: 'Node js', image: '/node-js.png', width: 80, height: 80 },
+  { name: 'Mongo db', image: '/mongodb-svgrepo-com.svg', width: 80, height: 80 },
+  { name: 'Docker', image: '/docker.webp', width: 80, height: 80 },
+  { name: 'Prisma', image: '/light-prisma-svgrepo-com.svg', width: 80, height: 80 },
+  { name: 'Redis', image: '/redis-svgrepo-com.svg', width: 80, height: 80 }
 ]
