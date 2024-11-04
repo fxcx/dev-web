677e4258aacb46a7a28b0d458b18edcf6bd44d1d
fxcx <facundo19burgos@gmail.com>
2024-11-04 12:00:47 -0300

    add type de navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,30 +2,42 @@
 import { NavLinks } from '@/types/navegations'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Dispatch, SetStateAction } from 'react'
 import Transition from './Transition'
+
+export interface HandleNavigationChangeProps {
+  prevPath: string
+  path: string
+  setIsRouting: Dispatch<SetStateAction<boolean>>
+  setPrevPath: Dispatch<SetStateAction<string>>
+}
+
+export const handleNavigationChange = ({
+  prevPath,
+  path,
+  setIsRouting,
+  setPrevPath
+}: HandleNavigationChangeProps): (() => void) | void => {
+  if (prevPath !== path) {
+    setIsRouting(true)
+    const timeout = setTimeout(() => {
+      setIsRouting(false)
+      setPrevPath(path)
+    }, 1200)
+
+    return () => clearTimeout(timeout)
+  }
+}
 
 const Navigation = (): JSX.Element => {
   const [isRouting, setIsRouting] = useState(false)
   const path = usePathname()
   const [prevPath, setPrevPath] = useState('/')
 
-  const handleNavigationChange = async (): Promise => {
-    if (prevPath !== path) {
-      setIsRouting(true)
-      const timeout = setTimeout(() => {
-        setIsRouting(false)
-        setPrevPath(path)
-      }, 1200)
-
-      return () => clearTimeout(timeout)
-    }
-  }
-
   useEffect(() => {
-    handleNavigationChange()
-  })
-
+    const cleanup = handleNavigationChange({ prevPath, path, setIsRouting, setPrevPath })
+    return cleanup
+  }, [path, prevPath])
   return (
     <div
       style={{ left: '25%' }}
