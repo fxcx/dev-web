edf3c0a731c421848f2d59109b70986fe430c269
fxcx <facundo19burgos@gmail.com>
2024-11-04 16:33:07 -0300

    add <p> description from filter projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,6 +10,7 @@ const Projects = (): JSX.Element => {
         <h1 className=' text-center text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 pt-20 pb-5'>
           Mis Proyectos
         </h1>
+        <p className='text-center text-sm font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 pb-5'>puede selecionar el proyecto por tecnologias </p>
       </div>
       <div />
       <section className='grid place-items-center overflow-x-hidden mb-[70px]'>
