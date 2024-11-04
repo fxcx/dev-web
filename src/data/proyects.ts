923b5424d8b46a86d6ecea5ae380d2eeb4d5ea82
fxcx <facundo19burgos@gmail.com>
2024-11-04 16:38:01 -0300

    add data all project

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -1,11 +1,11 @@
 import { Project } from '@/types/index'
-import { SiNextdotjs, SiTypescript, SiTailwindcss, SiPrisma, SiPostgresql, SiExpress, SiMongodb } from 'react-icons/si'
+import { SiNextdotjs, SiTypescript, SiTailwindcss, SiPrisma, SiPostgresql, SiExpress, SiMongodb, SiJavascript, SiHtml5, SiCss3 } from 'react-icons/si'
 export const projects: Project[] = [
   {
     id: 1,
-    name: 'Next.js Portfolio',
-    src: '/projects/port.jpg',
-    description: 'A personal portfolio built with Next.js, showcasing projects, skills, and experience.',
+    name: 'Portfolio',
+    src: '/projects/port.png',
+    description: 'Un portafolio personal creado con Next.js, donde muestro proyectos, habilidades y experiencias que me han aportado valor.',
     tags: [
       { name: 'Next.js', icon: SiNextdotjs },
       { name: 'TypeScript', icon: SiTypescript },
@@ -15,24 +15,21 @@ export const projects: Project[] = [
   },
   {
     id: 2,
-    name: 'Memealo',
-    src: '/projects/memealo.jpg',
-    description: 'A meme-based card game with a multiplayer setup, built for fun interactions with friends.',
+    name: 'Subaru prolineService',
+    src: '/projects/subaru.png',
+    description: 'Un servicio de taller y venta de autos donde desarrollé su página oficial y despliegue web.',
     tags: [
-      { name: 'Next.js', icon: SiNextdotjs },
-      { name: 'TypeScript', icon: SiTypescript },
-      { name: 'Prisma', icon: SiPrisma },
-      { name: 'PostgreSQL', icon: SiPostgresql },
-      { name: 'Express.js', icon: SiExpress },
-      { name: 'Tailwind', icon: SiTailwindcss }
+      { name: 'JS', icon: SiJavascript },
+      { name: 'html', icon: SiHtml5 },
+      { name: 'css', icon: SiCss3 }
     ],
-    link: '/memealo'
+    link: 'https://github.com/fxcx/Taller_Subaru'
   },
   {
     id: 3,
     name: 'Edulink',
     src: '/projects/edulink.webp',
-    description: 'An educational platform that connects mentors and students to facilitate learning.',
+    description: 'Una plataforma educativa que conecta mentores y estudiantes, facilitando el aprendizaje y priorizando la suscripción y el seguimiento del progreso.',
     tags: [
       { name: 'Next.js', icon: SiNextdotjs },
       { name: 'TypeScript', icon: SiTypescript },
@@ -41,6 +38,33 @@ export const projects: Project[] = [
       { name: 'Express.js', icon: SiExpress },
       { name: 'Tailwind', icon: SiTailwindcss }
     ],
-    link: '/edulink'
+    link: 'https://gitlab.com/sistema-de-gestion-de-inscripciones/sistema-inscripciones-react'
+  },
+  {
+    id: 4,
+    name: 'Reves',
+    src: '/projects/reves.png',
+    description: 'Una página de servicios y venta de productos que ofrece revestimiento para muros y pisos, permitiendo a los clientes elegir colores, contactarse con el proveedor y adquirir productos de su propia marca o de su elección.',
+    tags: [
+      { name: 'Next.js', icon: SiNextdotjs },
+      { name: 'TypeScript', icon: SiTypescript },
+      { name: 'Tailwind', icon: SiTailwindcss }
+    ],
+    link: 'https://github.com/fxcx/front-revestimiento'
+  },
+  {
+    id: 5,
+    name: 'Memealo',
+    src: '/projects/memealo.jpg',
+    description: 'Uno de mis primeros proyectos, donde trabajé en un equipo de 17 personas, desarrollando el backend y frontend con roles rotativos.',
+    tags: [
+      { name: 'Next.js', icon: SiNextdotjs },
+      { name: 'TypeScript', icon: SiTypescript },
+      { name: 'Prisma', icon: SiPrisma },
+      { name: 'PostgreSQL', icon: SiPostgresql },
+      { name: 'Express.js', icon: SiExpress },
+      { name: 'Tailwind', icon: SiTailwindcss }
+    ],
+    link: 'https://github.com/fxcx/memealo-be/tree/develop'
   }
 ]
