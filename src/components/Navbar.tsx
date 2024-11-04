48d0388471fcae7222d9889f21ef0029074f00e9
fxcx <facundo19burgos@gmail.com>
2024-11-04 15:45:37 -0300

    add gittlab icon social

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,33 +6,33 @@ const Navbar = (): JSX.Element => {
   return (
     <div className='absolute top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between flex-block px-10 md:px-20'>
       <div className='flex flex-row gap-3 items-center'>
-        <h1 className='text-white text-[25px] font-semibold'>
-          facundo -{' '}
-          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>
-            {' '}
-            Burgos{' '}
-          </span>
-        </h1>
+        <Link href='/'>
+          <h1 className='text-white text-[25px] font-semibold'>
+            facundo -{' '}
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>
+              {' '}
+              Burgos{' '}
+            </span>
+          </h1>
+        </Link>
       </div>
 
       <div className='flex gap-4'>
         {Socials.map((social) => (
-          <div className='py-4' key={social.name}>
-            <Image
-              src={social.src}
-              alt={social.name}
-              width={38}
-              height={38}
-            />
-
-            <Link
-              className='opacity-0 relative right-9'
-              key={social.name}
-              href={social.link}
-            >
-              {social.name}
-            </Link>
-          </div>
+          <Link
+            className='py-2 relative right-8'
+            key={social.name}
+            href={social.link}
+          >
+            <div className='py-4'>
+              <Image
+                src={social.src}
+                alt={social.name}
+                width={38}
+                height={38}
+              />
+            </div>
+          </Link>
         ))}
       </div>
     </div>
--- src/data/social.ts
+++ src/data/social.ts
@@ -1,6 +1,7 @@
 import { SocialLink } from '@/types/index'
 
 export const socials: SocialLink[] = [
-  { name: 'github', src: '/github-color-svgrepo-com.svg', link: 'https://github.com/fxcx' },
-  { name: 'Linkedin', src: '/linkedin-svgrepo-com.svg', link: 'https://www.linkedin.com/in/facundoburgos96/' }
+  { name: 'github', src: '/social/github-color-svgrepo-com.svg', link: 'https://github.com/fxcx' },
+  { name: 'Linkedin', src: '/social/linkedin-svgrepo-com.svg', link: 'https://www.linkedin.com/in/facundoburgos96/' },
+  { name: 'Gitlab', src: '/social/gitlab-logo-500.svg', link: 'https://gitlab.com/fxcx' }
 ]
